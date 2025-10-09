12fdfd4e1598d85971e88beb793de3d79f889dc2
Ziyi Qiu <qiuzy21@mails.tsinghua.edu.cn>
2025-10-09 13:38:33 +0800

    config

--- scripts/history_spec/run_grpo.sh
+++ scripts/history_spec/run_grpo.sh
@@ -48,7 +48,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.enforce_eager=True \
     actor_rollout_ref.rollout.free_cache_engine=False \
     actor_rollout_ref.rollout.n=4 \
-    actor_rollout_ref.rollout.use_history_spec_decode=True \
+    actor_rollout_ref.rollout.use_history_spec_decode=False \
     critic.optim.lr=1e-5 \
     critic.model.use_remove_padding=True \
     critic.model.path=Qwen/Qwen2.5-0.5B-Instruct \
