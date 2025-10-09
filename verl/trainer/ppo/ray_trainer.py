b27a0de6b3e88de5140384f050194e79fc29a9ad
QiuZiyi <qiuzy21@mails.tsinghua.edu.cn>
2025-10-09 15:37:40 +0800

    debug

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -1293,7 +1293,8 @@ class RayPPOTrainer:
                                 
                                 token_level_scores = batch_item.batch["token_level_scores"]
                                 response = batch_item.batch["responses"]
-                                prompt_id = str(hash(tuple(batch_item.batch["prompts"].numpy().tolist())))
+                                prompt_id = str(hash(tuple(batch_item.non_tensor_batch["responses"])))
+                                print(f"vllm input token ids: {batch_item.non_tensor_batch["responses"]}")
                                 if prompt_id not in self.history_rollout_tree_dict:
                                     self.history_rollout_tree_dict[prompt_id] = RewardAwareSuffixTree()
                                 else:
